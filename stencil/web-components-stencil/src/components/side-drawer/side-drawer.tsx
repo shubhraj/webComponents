79475c53c793ee9325f44e7998e63743bd7fa5de
shubham_patil <shubham.patil@cdk.com>
2021-01-18 10:55:06 +0530

    adding a stencil component

---
+++ stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,65 @@
+import { Component, h, Prop, State, Method } from "@stencil/core";
+
+@Component({
+    tag: 'wc-side-drawer',
+    shadow: true,
+    styleUrl: './side-drawer.css' 
+})
+
+export class SideDrawer {
+    @State() showContactInfo = false;
+    @Prop({reflect: true}) title: string;
+    @Prop({reflect: true}) open: boolean;
+
+    onCloseDrawer(){
+        this.open = false;
+    }
+
+    onContentChange(content: string){
+        this.showContactInfo = content === 'contact';
+    }
+
+    @Method()
+    openDrawer(){
+        this.open = true
+    }
+
+    render(){
+        let mainContent = <slot/>;
+        if(this.showContactInfo){
+            let contactUsContent = (
+                <div id='contact-info'>
+                    <h2>This is Contact Info</h2>
+                    <p>you can contact us by Phone or Email</p>
+                    <ul>
+                        <li>Phone : 23213213</li>
+                        <li>Email : <a href="mailto:something@something.com">something@something.com</a></li>
+                    </ul>
+                </div>
+            )
+            mainContent = contactUsContent;
+        }
+      
+        return [
+            <div class="backdrop" onClick={this.onCloseDrawer.bind(this)}></div>,
+            <aside>
+                <header>
+                    <h1>{this.title}</h1>
+                    <button id='close-btn' onClick={this.onCloseDrawer.bind(this)}>X</button>    
+                </header>
+                <section id="tabs">
+                    <button
+                    class = {!this.showContactInfo ? 'active': ''}
+                    onClick={this.onContentChange.bind(this,'nav')}>Navigation</button>
+                    <button 
+                    class = {this.showContactInfo ? 'active': ''}
+                    onClick={this.onContentChange.bind(this,'contact')}>Contact Us</button>
+                </section>
+                <main>
+                    {mainContent}
+                </main> 
+            </aside>
+        ];
+    }
+}
+
